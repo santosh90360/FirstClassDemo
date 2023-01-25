2298d0031d9bd0f5f96d9b34faaf9a85d1f37540
santosh90360 <santosh3033@gmail.com>
2023-01-25 13:00:44 +0530

    Add project files.

---
+++ FirstClassDemo/Registration.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Registration.aspx.cs" Inherits="FirstClassDemo.Registration" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="form-group">
+                <label>Name</label>
+                <asp:TextBox ID="txtName" runat="server" CssClass="form-control"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <asp:TextBox ID="txtEmail" runat="server" CssClass="form-control"></asp:TextBox>
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <asp:TextBox ID="txtPassword" runat="server" CssClass="form-control"></asp:TextBox>
+            </div>
+
+             <div class="form-group">
+                <label>Confirm Password</label>
+                <asp:TextBox ID="txtConfirmPassword" runat="server" CssClass="form-control"></asp:TextBox>
+            </div>
+
+             <div class="form-group">             
+                <asp:Button ID="btnRegister" runat="server" Text="Register" OnClick="btnRegister_Click" />
+            </div>
+
+             <div class="form-group">
+                <asp:Label ID="lblMesssage" runat="server"></asp:Label>
+            </div>
+        </div>
+
+    </div>
+</asp:Content>
---
+++ FirstClassDemo/Registration.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+
+namespace FirstClassDemo
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            bool result = CreateNewuser();
+            if (result==true)
+            {
+                lblMesssage.Text = "Success! You are registered successfully.";
+                lblMesssage.ForeColor = Color.Green;
+            }
+            else
+            {
+                lblMesssage.Text = "Failed! You are notregister successfully.";
+                lblMesssage.ForeColor = Color.Red;
+            }
+        }
+
+        private bool CreateNewuser()
+        {
+            bool result = false;
+            try
+            {
+                string connString = @"Data Source=servername; database=dbAnkitDemo; integrated security=true;";
+                using (SqlConnection connection = new SqlConnection(connString))
+                {
+                    SqlCommand cmd = new SqlCommand("insert into Registration(Name,Email,Password) values(@Name,@Email,@Password)", connection);
+                    cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                    cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+                    connection.Open();
+                    int temp = cmd.ExecuteNonQuery(); // insert, update, delete
+                    connection.Close();
+                    if (temp > 0)
+                    {
+                        result = true;
+                    }
+                    else
+                    {
+                        result = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
